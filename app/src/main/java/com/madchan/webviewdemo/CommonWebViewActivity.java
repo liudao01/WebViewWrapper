44c3add2a21fcaed755e7ebce1f7a955ebaf377b
liuml <liudao1@qq.com>
2022-08-15 14:22:02 +0800

    加载webveiw成功

--- app/src/main/java/com/madchan/webviewdemo/CommonWebViewActivity.java
+++ app/src/main/java/com/madchan/webviewdemo/CommonWebViewActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -18,7 +19,8 @@ import android.widget.Toast;
 
 public class CommonWebViewActivity extends Activity {
 
-    public static final String TEST_URL = "http://blog.csdn.net/liudao7994?skin=dark1";
+    //    public static final String TEST_URL = "http://blog.csdn.net/liudao7994?skin=dark1";
+    public static final String TEST_URL = "http://10.26.2.109:8080/pos-html/login?v=6384223247";
     private static final String INTENT_EXTRA_URL = "INTENT_EXTRA_URL";
     private String url;
 
@@ -47,7 +49,25 @@ public class CommonWebViewActivity extends Activity {
 
     private void initWebView() {
         wvCommon = (WebView) findViewById(R.id.wv_common);
-        wvCommon.getSettings().setJavaScriptEnabled(true);
+        WebSettings settings = wvCommon.getSettings();
+        WebSettings webSettings = wvCommon.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+//         当网页需要保存数时据,设置下面属性
+        webSettings.setDomStorageEnabled(true);
+
+        //设置页面自适应 窗口大小改变后页面自适应改变
+        webSettings.setUseWideViewPort(true);//设置为使用webview推荐的窗口，主要是为了配合下一个属性
+        webSettings.setLoadWithOverviewMode(true);//设置网页自适应屏幕大小，该属性必须和上一属性配合使用
+
+//        webSettings.setGeolocationEnabled(true); //启用还H5的地理定位服务
+//         设置是否允许webview使用缩放的功能
+        webSettings.setSupportZoom(false);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setDisplayZoomControls(false); //隐藏webview缩放按钮
+
+//         提高网页渲染的优先级
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
         wvCommon.addJavascriptInterface(new WebAppInterface(getApplicationContext()), "Android");
         wvCommon.setWebViewClient(new WebViewClient());
         wvCommon.setWebViewClient(new WebViewClient() {
